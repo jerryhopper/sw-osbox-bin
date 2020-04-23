7aec84170cb3cb354fcb9010bb88674a12212a73
jerryhopper <hopper.jerry@gmail.com>
2020-04-24 01:13:26 +0200

    fixed systemd service executable

--- install.sh
+++ install.sh
@@ -180,7 +180,7 @@ git clone https://github.com/jerryhopper/sw-osbox-core.git
 cd $CURDIR
 
 
-
+PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d
 /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini /usr/local/osbox/bin/composer.phar --working-dir=/usr/local/osbox/project/sw-osbox-core install
 
 
