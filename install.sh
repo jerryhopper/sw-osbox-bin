d8efc6c26a54eba11ba217d38ffcae8a71fe3752
jerryhopper <hopper.jerry@gmail.com>
2020-04-24 13:07:39 +0200

    checks

--- install.sh
+++ install.sh
@@ -210,7 +210,7 @@ PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d /usr/local/osbox/bin/osboxd -c /usr
 
 
 echo "Running composer..."
-PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini /usr/local/osbox/bin/composer.phar install
+PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini --working-dir=/usr/local/osbox/project/sw-osbox-core /usr/local/osbox/bin/composer.phar install
 
 #PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d
 #/usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini /usr/local/osbox/bin/composer.phar --working-dir=/usr/local/osbox/project/sw-osbox-core install
