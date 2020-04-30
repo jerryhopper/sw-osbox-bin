b87269a7d94f6b03805662d01f53f08c8a42a164
jerryhopper <hopper.jerry@gmail.com>
2020-04-30 16:05:58 +0200

    create db on install

--- install.sh
+++ install.sh
@@ -211,7 +211,7 @@ PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d /usr/local/osbox/bin/osboxd -c /usr
 #/usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini /usr/local/osbox/bin/composer.phar --working-dir=/usr/local/osbox/project/sw-osbox-core install
 
 
-if [ ! -f /etc/osbox/osbox.db ]; else
+if [ ! -f /etc/osbox/osbox.db ]; then
     touch /etc/osbox/osbox.db
     chown osbox:osbox /etc/osbox/osbox.db
 fi
