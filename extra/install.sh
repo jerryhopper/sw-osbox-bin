5e07b91eac9f016678cb42b05ef945c6f00a1592
jerryhopper <hopper.jerry@gmail.com>
2020-09-25 15:50:31 +0200

    installerfix

--- extra/install.sh
+++ extra/install.sh
@@ -234,15 +234,6 @@ chmod +x /sbin/osbox
 
 
 
-if [ -d /var/osbox ]; then
-  rm -rf /var/osbox
-fi
-mkdir -p /var/osbox
-mkfifo /var/osbox/mypipe
-
-cp  /usr/local/osbox/lib/systemd/osbox-pipe.service /etc/systemd/system
-
-
 
 osbox install
 
