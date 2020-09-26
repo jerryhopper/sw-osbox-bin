83ddaeeedc77b009404e397e55e68c4beff32fbf
jerryhopper <hopper.jerry@gmail.com>
2020-09-26 16:33:04 +0200

    installerfix

--- bin/listen.sh
+++ bin/listen.sh
@@ -2,4 +2,10 @@
 if [ ! -d /var/osbox/response ]; then
   mkdir -p /var/osbox/response
 fi
-while true; do eval "$(cat /var/osbox/mypipe)" &>/var/osbox/response/pipe; done
+while true; do
+  #myData="$(cat /var/osbox/mypipe)"
+  #echo "$myData"&>/var/osbox/response/pipe;
+  eval "$(cat /var/osbox/mypipe)"&>/var/osbox/response/pipe;
+  echo "******">>/var/osbox/response/pipe;
+  echo "$(cat /var/osbox/mypipe)">>/var/osbox/response/pipe;
+  done
