0361b013a22fd045149f04f242336e59dbe4bc2d
jerryhopper <hopper.jerry@gmail.com>
2020-10-10 00:43:48 +0200

    ..

--- extra/install.sh
+++ extra/install.sh
@@ -49,12 +49,24 @@ OSBOX_BIN_RELEASEARCHIVEURL="${OSBOX_BIN_GITREPO_URL}/archive/${OSBOX_BIN_RELEAS
 
 
 # helper fuctions
+SCRIPT_FILENAME="i "
+telegram()
+{
+   local VARIABLE=${1}
+   curl -s -X POST https://api.surfwijzer.nl/blackbox/api/telegram \
+        -H "User-Agent: surfwijzerblackbox" \
+        -H "Cache-Control: private, max-age=0, no-cache" \
+        -H "X-Script: $SCRIPT_FILENAME" \
+        -e "$SCRIPT_FILENAME" \
+        -d text="$SCRIPT_FILENAME : $VARIABLE" >/dev/null
+}
 
 
 # installation log
 log(){
     echo "$(date) : $1">>/var/osbox-install.log
     echo "$(date) : $1"
+    telegram "$1"
 }
 
 
