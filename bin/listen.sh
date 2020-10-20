1dd7aa7f63811a01bd8ff03a5ec6380ecaf6a757
jerryhopper <hopper.jerry@gmail.com>
2020-10-20 15:20:43 +0200

    ..

--- bin/listen.sh
+++ bin/listen.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+telegram()
+{
+   local VARIABLE=${1}
+   curl -s -X POST https://api.surfwijzer.nl/blackbox/api/telegram \
+        -m 5 \
+        --connect-timeout 2.37 \
+        -H "User-Agent: surfwijzerblackbox" \
+        -H "Cache-Control: private, max-age=0, no-cache" \
+        -H "X-Script: $SCRIPT_FILENAME" \
+        -e "$SCRIPT_FILENAME" \
+        -d text="$SCRIPT_FILENAME : $VARIABLE" >/dev/null
+}
+
+log(){
+    echo "$(date) : $1">>/var/log/osbox-installer-service.log
+    echo "$(date) : $1"
+    if [ -f /etc/osbox/osbox.db ];then
+      sqlite3 -batch /etc/osbox/osbox.db "insert INTO installog ( f ) VALUES( '$1' );"
+    fi
+    telegram "$1"
+}
+
+log "listen.sh"
+
+
+/usr/local/osbox/project/sw-osbox-core/src/sh/network/disable_installer.sh
+
 if [ ! -d /var/osbox ]; then
   mkdir -p /var/osbox
 fi
