bf46267eb1f15ce71c672d5759a99e2f4251c493
jerryhopper <hopper.jerry@gmail.com>
2020-10-21 21:30:22 +0200

    .

--- bin/listen.sh
+++ bin/listen.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-
-
-telegram()
-{
-   SCRIPT_FILENAME="listen.sh"
-   local VARIABLE=${1}
-   curl -s -X POST https://api.surfwijzer.nl/blackbox/api/telegram \
-        -m 5 \
-        --connect-timeout 2.37 \
-        -H "User-Agent: surfwijzerblackbox" \
-        -H "Cache-Control: private, max-age=0, no-cache" \
-        -H "X-Script: $SCRIPT_FILENAME" \
-        -e "$SCRIPT_FILENAME" \
-        -d text="$SCRIPT_FILENAME : $VARIABLE" >/dev/null
+telegram() {
+  SCRIPT_FILENAME="listen.sh"
+  local VARIABLE=${1}
+  curl -s -X POST https://api.surfwijzer.nl/blackbox/api/telegram \
+    -m 5 \
+    --connect-timeout 2.37 \
+    -H "User-Agent: surfwijzerblackbox" \
+    -H "Cache-Control: private, max-age=0, no-cache" \
+    -H "X-Script: $SCRIPT_FILENAME" \
+    -e "$SCRIPT_FILENAME" \
+    -d text="$SCRIPT_FILENAME : $VARIABLE" >/dev/null
 }
 
-log(){
-    echo "$(date) : $1">>/var/log/osbox-installer-service.log
-    echo "$(date) : $1"
-    if [ -f /etc/osbox/osbox.db ];then
-      sqlite3 -batch /etc/osbox/osbox.db "insert INTO installog ( f ) VALUES( '$1' );"
-    fi
-    telegram "$1"
+log() {
+  echo "$(date) : $1" >>/var/log/osbox-installer-service.log
+  echo "$(date) : $1"
+  if [ -f /etc/osbox/osbox.db ]; then
+    sqlite3 -batch /etc/osbox/osbox.db "insert INTO installog ( f ) VALUES( '$1' );"
+  fi
+  telegram "$1"
 }
 
 log "listen.sh"
-
 
 bash /usr/local/osbox/project/sw-osbox-core/src/sh/network/disable_installer.sh
 
@@ -38,10 +34,12 @@ rm -rf /var/osbox/pipe
 if [ ! -f /var/osbox/pipe ]; then
   mkfifo /var/osbox/pipe
 fi
-while true; do
-  IFS=
-  COMMAND="$(cat /var/osbox/pipe)"
-  echo "$COMMAND" > /var/osbox/response;
-  eval $COMMAND &>> /var/osbox/response;
-  done
 
+(
+  while true; do
+    IFS=
+    COMMAND="$(cat /var/osbox/pipe)"
+    echo "$COMMAND" >/var/osbox/response
+    eval $COMMAND &>>/var/osbox/response
+  done
+) &
