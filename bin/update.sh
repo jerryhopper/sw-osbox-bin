59b030756dab41986503c52438990f9ec26ef09a
jerryhopper <hopper.jerry@gmail.com>
2020-11-18 21:41:12 +0100

    created functions

--- bin/update.sh
+++ bin/update.sh
@@ -2,104 +2,19 @@
 
 
 
+source /usr/local/osbox/bin/fn/GetLatestVersion.fn
 
-# is_command function
-is_command() {
-    # Checks for existence of string passed in as only function argument.
-    # Exit value of 0 when exists, 1 if not exists. Value is the result
-    # of the `command` shell built-in call.
-    local check_command="$1"
-    command -v "${check_command}" >/dev/null 2>&1
-}
+source /usr/local/osbox/bin/fn/GetRemoteVersion.fn
 
+source /usr/local/osbox/bin/fn/InstallSwoole.fn
 
-DownloadLatest(){
-      _ORG_NAME=$1
-      _REPO_NAME=$2
-      _LATEST_VERSION=$3
-      _BIN_DIR=$4
+source /usr/local/osbox/bin/fn/DownloadUnpack.fn
 
-      echo "Downloading ${_ORG_NAME}/${_REPO_NAME} latest"
-      #https://github.com/jerryhopper/sw-osbox-bin/archive/master.zip
+source /usr/local/osbox/bin/fn/Install.fn
 
-      # Check the download url, if it responds with 200
-      DOWNLOAD_CODE=$(curl -L -s -o /dev/null -I -w "%{http_code}" --silent --output /dev/null  https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/master.tar.gz)
-      if [ "$DOWNLOAD_CODE" != "200" ];then
-        log "Download error! ( ${DOWNLOAD_CODE} ) https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/master.tar.gz"
-              exit 1
-      fi
+source /usr/local/osbox/bin/fn/log.fn
 
-      # Download the file
-      curl -L -o master.tar.gz https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/master.tar.gz &> /dev/null
-      if [ ! -d ${_BIN_DIR} ];then
-        mkdir -p ${_BIN_DIR}
-      fi
-      tar -C ${_BIN_DIR} -xf master.tar.gz --strip 1 > /dev/null
-      rm -rf master.tar.gz
-      echo "ok"
-}
-
-
-GetRemoteVersion(){
-      _ORG_NAME=$1
-      _REPO_NAME=$2
-      if ! is_command "jq"; then
-        LATEST_VERSION=$(curl -s https://api.github.com/repos/${_ORG_NAME}/${_REPO_NAME}/releases/latest | grep "tag_name" | cut -d'v' -f2 | cut -d'"' -f4)
-      else
-        LATEST_VERSION=$(curl -s https://api.github.com/repos/${_ORG_NAME}/${_REPO_NAME}/releases/latest|jq .tag_name -r )
-      fi
-      echo $LATEST_VERSION
-}
-
-DownloadUnpack(){
-      _ORG_NAME=$1
-      _REPO_NAME=$2
-      _LATEST_VERSION=$3
-      _BIN_DIR=$4
-
-      echo "https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/${_LATEST_VERSION}.tar.gz"
-      # Check the download url, if it responds with 200
-      DOWNLOAD_CODE=$(curl -L -s -o /dev/null -I -w "%{http_code}" https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/${_LATEST_VERSION}.tar.gz)
-      if [ "$DOWNLOAD_CODE" != "200" ];then
-        log "Download error! ( ${DOWNLOAD_CODE} ) "https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/${_LATEST_VERSION}.tar.gz""
-              exit 1
-      fi
-
-      # Download the file
-      curl -s -L -o ${_REPO_NAME}.tar.gz https://github.com/${_ORG_NAME}/${_REPO_NAME}/archive/${_LATEST_VERSION}.tar.gz &> /dev/null
-      if [ ! -d ${_BIN_DIR} ];then
-        mkdir -p ${_BIN_DIR}
-      fi
-      tar -C ${_BIN_DIR} -xf ${_REPO_NAME}.tar.gz --strip 1 > /dev/null
-      rm -rf ${_REPO_NAME}.tar.gz
-      echo "ok"
-}
-
-
-telegram()
-{
-   SCRIPT_FILENAME="install.sh"
-   local VARIABLE=${1}
-   curl -s -X POST https://api.surfwijzer.nl/blackbox/api/telegram \
-        -H "User-Agent: surfwijzerblackbox" \
-        -H "Cache-Control: private, max-age=0, no-cache" \
-        -H "X-Script: $SCRIPT_FILENAME" \
-        -e "$SCRIPT_FILENAME" \
-        -d text="$SCRIPT_FILENAME : $VARIABLE" >/dev/null
-}
-
-
-# installation log
-log(){
-    echo "$(date) : $1">>/var/osbox-install.log
-    echo "$(date) : $1"
-    if [ -f /etc/osbox/osbox.db ];then
-      sqlite3 -batch /etc/osbox/osbox.db "insert INTO installog ( f ) VALUES( '$1' );"
-    fi
-    telegram "$1"
-}
-
-
+source /usr/local/osbox/bin/fn/is_command.fn
 
 
 
@@ -123,6 +38,82 @@ if [[ ! $EUID -eq 0 ]];then
     exit 1
   fi
 fi
+
+
+
+
+log "Installation script sw-osbox-bin"
+
+
+
+
+
+# Root check
+if [[ ! $EUID -eq 0 ]];then
+  if [[ -x "$(command -v sudo)" ]]; then
+    exec sudo bash "$0" "$@"
+    exit $?
+  else
+    log "   sudo is needed to run the installer.  Please run this script as root or install sudo."
+    exit 1
+  fi
+fi
+
+# Check and install requirements.
+bash /usr/local/osbox/bin/checkrequirements.sh
+
+
+
+if [ ! $(id -u osbox) ];then
+  log "Adding osbox user"
+  useradd -m -c "osbox user account" osbox
+fi
+
+INSTALL_MODE="release"
+# check if using latest versions
+if [ -f /etc/osbox/.dev ];then
+   INSTALL_MODE="latest"
+fi
+
+# Install the applications
+Install "jerryhopper" "sw-osbox-bin" "/usr/local/osbox" "$INSTALL_MODE"
+
+Install "jerryhopper" "sw-osbox-core" "/usr/local/osbox/project/sw-osbox-core" "$INSTALL_MODE"
+
+# Check and set permissions
+bash /usr/local/osbox/bin/checkpermissions.sh
+
+## Enable the service
+if [ ! -f /etc/systemd/system/osbox.service ];then
+  ln -s /usr/local/osbox/lib/systemd/osbox.service /etc/systemd/system/osbox.service
+fi
+
+# Flag service active
+if [ ! -f /etc/systemd/system/multi-user.target.wants/osbox.service ];then
+  ln -s /usr/local/osbox/lib/systemd/osbox.service /etc/systemd/system/multi-user.target.wants/osbox.service
+fi
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
