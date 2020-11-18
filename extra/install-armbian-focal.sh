7c9e0b5fab06b4ad513485f511d9bfbfddc6f17a
jerryhopper <hopper.jerry@gmail.com>
2020-11-18 15:00:17 +0100

    reworked install func

--- extra/install-armbian-focal.sh
+++ extra/install-armbian-focal.sh
@@ -37,17 +37,44 @@ is_command() {
     command -v "${check_command}" >/dev/null 2>&1
 }
 
-GetRemoteVersion(){
-      ORG_NAME=$1
-      REPO_NAME=$2
-
+GetLatestVersion(){
+      _ORG_NAME=$1
+      _REPO_NAME=$2
       if ! is_command "jq"; then
-        LATEST_VERSION=$(curl -s https://api.github.com/repos/${ORG_NAME}/${REPO_NAME}/releases/latest | grep "tag_name" | cut -d'v' -f2 | cut -d'"' -f4)
+        LATEST_VERSION=$(curl -s https://api.github.com/repos/${_ORG_NAME}/${_REPO_NAME}/git/refs/heads/master | grep "sha" | cut -d'v' -f2 | cut -d'"' -f4)
       else
-        LATEST_VERSION=$(curl -s https://api.github.com/repos/${ORG_NAME}/${REPO_NAME}/releases/latest|jq .tag_name -r )
+        LATEST_VERSION=$(curl -s https://api.github.com/repos/${_ORG_NAME}/${_REPO_NAME}/git/refs/heads/master|jq .object.sha -r )
       fi
       echo $LATEST_VERSION
 }
+
+GetRemoteVersion(){
+      _ORG_NAME=$1
+      _REPO_NAME=$2
+      if ! is_command "jq"; then
+        LATEST_VERSION=$(curl -s https://api.github.com/repos/${_ORG_NAME}/${_REPO_NAME}/releases/latest | grep "tag_name" | cut -d'v' -f2 | cut -d'"' -f4)
+      else
+        LATEST_VERSION=$(curl -s https://api.github.com/repos/${_ORG_NAME}/${_REPO_NAME}/releases/latest|jq .tag_name -r )
+      fi
+      echo $LATEST_VERSION
+}
+
+
+InstallSwoole(){
+	# SWOOLE
+	log "Cloning and compiling swoole"
+	git clone https://github.com/swoole/swoole-src.git && cd swoole-src
+	git checkout v4.5.5
+	phpize && ./configure --enable-sockets --enable-openssl && ! make && make install
+	log "Installing swoole"
+	echo "extension=swoole.so" >> $(php -i | grep php.ini|grep Loaded | awk '{print $5}')
+
+	log  "Remove unneccesary files"
+	cd .. && rm -rf ./swoole-src
+}
+
+
+## https://github.com/jerryhopper/sw-osbox-bin/archive/b538ec55a7487c6e216a563d38a5e4facf66b6df.tar.gz
 
 
 # DownloadUnpack "jerryhopper" "sw-osbox" "0.1" "/usr/local/osbox"
@@ -73,30 +100,14 @@ DownloadUnpack(){
 
       tar -C ${BIN_DIR} -xvf ${REPO_NAME}.tar.gz --strip 1
       rm -rf ${REPO_NAME}.tar.gz
-      #OSBOX_BIN_LOCALVERSION="$(</etc/osbox/.osbox.bin.version)"
+
 }
-
-
-InstallSwoole(){
-	# SWOOLE
-	log "Cloning and compiling swoole"
-	git clone https://github.com/swoole/swoole-src.git && cd swoole-src
-	git checkout v4.5.5
-	phpize && ./configure --enable-sockets --enable-openssl && ! make && make install
-	log "Installing swoole"
-	echo "extension=swoole.so" >> $(php -i | grep php.ini|grep Loaded | awk '{print $5}')
-
-	log  "Remove unneccesary files"
-	cd .. && rm -rf ./swoole-src
-}
-
-
-
 
 function Install (){
   _ORGNAME=$1
   _REPONAME=$2
   _DSTFOLDER=$3
+  _INSTALL_MODE=$4
 
   # check and create /etc/osbox
   if [ ! -d /etc/osbox ]; then
@@ -109,8 +120,15 @@ function Install (){
       echo "0">"$_VERSIONFILE"
   fi
 
+
   PACKAGE_LOCALVERSION="$(<$_VERSIONFILE)"
-  PACKAGE_REMOTEVERSION="$(GetRemoteVersion '$_ORGNAME' '$_REPONAME')"
+
+  if [ "$_INSTALL_MODE"=="latest" ];then
+    PACKAGE_REMOTEVERSION="$(GetLatestVersion '$_ORGNAME' '$_REPONAME')"
+  else
+    PACKAGE_REMOTEVERSION="$(GetRemoteVersion '$_ORGNAME' '$_REPONAME')"
+  fi
+
 
   if [ "$PACKAGE_REMOTEVERSION"=="" ];then
     echo "Unexpected version error"
@@ -148,32 +166,33 @@ if [[ ! $EUID -eq 0 ]];then
   fi
 fi
 
-
+# Check and install requirements.
 bash /usr/local/osbox/bin/checkrequirements.sh
-
 
 log "Adding osbox user"
 useradd -m -c "osbox user account" osbox
 
-
-
+INSTALL_MODE="release"
+# check if using latest versions
+if [ -f /etc/osbox/.dev ];then
+   INSTALL_MODE="latest"
+fi
 
 # Install the applications
-Install "jerryhopper" "sw-osbox-bin" "/usr/local/osbox"
-Install "jerryhopper" "sw-osbox-core" "/usr/local/osbox/project/sw-osbox-core"
+Install "jerryhopper" "sw-osbox-bin" "/usr/local/osbox" "$INSTALL_MODE"
+Install "jerryhopper" "sw-osbox-core" "/usr/local/osbox/project/sw-osbox-core" "$INSTALL_MODE"
 
+# Check and set permissions
+bash /usr/local/osbox/bin/checkpermissions.sh
 
 ## Enable the service
 if [ ! -f /etc/systemd/system/osbox.service ];then
   ln -s /usr/local/osbox/lib/systemd/osbox.service /etc/systemd/system/osbox.service
 fi
+
 # Flag service active
 if [ ! -f /etc/systemd/system/multi-user.target.wants/osbox.service ];then
   ln -s /usr/local/osbox/lib/systemd/osbox.service /etc/systemd/system/multi-user.target.wants/osbox.service
 fi
-
-echo "checkpermissions"
-bash /usr/local/osbox/bin/checkpermissions.sh
-
 
 exit 0
